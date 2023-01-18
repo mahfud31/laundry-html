c0081634ee3d2a77a81c181fa94960f07b3eb248
mahfud31 <mahfudelchotibi02@gmail.com>
2023-01-18 16:46:52 +0700

    pages layanan, transaksi, pengambilan

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,3 +11,21 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height() - 20);
   });
 });
+
+// amount button
+$(".quantity-button")
+  .off("click")
+  .on("click", function () {
+    if ($(this).hasClass("quantity-add")) {
+      var addValue = parseInt($(this).parent().find("input").val()) + 1;
+      $(this).parent().find("input").val(addValue).trigger("change");
+    }
+
+    if ($(this).hasClass("quantity-remove")) {
+      var removeValue = parseInt($(this).parent().find("input").val()) - 1;
+      if (removeValue == 0) {
+        removeValue = 1;
+      }
+      $(this).parent().find("input").val(removeValue).trigger("change");
+    }
+  });
